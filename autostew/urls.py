d9f1939cd0f7da686c45a93c179cd99b441ed6b8
Michael Wittig <witmic1@gmail.com>
2016-03-06 20:49:57 +0100

    update urls to use variable link

--- autostew/urls.py
+++ autostew/urls.py
@@ -5,7 +5,7 @@ from autostew_web_session.views import ListTracks, TrackView
 
 urlpatterns = [
     url(r'^session/', include('autostew_web_session.urls')),
-    url(r'^admin/', admin.site.urls),
     url(r'^tracks/$', ListTracks.as_view(), name='tracks'),
     url(r'^tracks/(?P<pk>[0-9]+)/?$', TrackView.as_view(), name='track'),
+    url(r'^admin/', admin.site.urls, name='admin'),
 ]
--- autostew_web_session/urls.py
+++ autostew_web_session/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 app_name = 'session'
 urlpatterns = [
-    url(r'^all/$', views.ListSessions.as_view(), name='sessions'),
+    url(r'^all/?$', views.ListSessions.as_view(), name='sessions'),
     url(r'^(?P<pk>[0-9]+)/?$', views.SessionView.as_view(), name='session'),
     url(r'^snapshot/(?P<pk>[0-9]+)/?$', views.SnapshotView.as_view(), name='snapshot'),
-    url(r'^create/$', views.CreateSessionView.as_view(), name='create_session'),
+    url(r'^create/?$', views.CreateSessionView.as_view(), name='create_session'),
 ]
