42b859aceba5cf45b3290e60972e30f5b7edd3e8
Michael Wittig <witmic1@gmail.com>
2016-03-07 12:05:53 +0100

    db_reader can access server's sessionsetups

--- autostew_back/plugins/db_reader.py
+++ autostew_back/plugins/db_reader.py
@@ -1,13 +1,24 @@
 from autostew_back.plugins import db
+from autostew_web_session.models import Server
 
 name = 'DB reader'
 dependencies = [db]
 
+server_in_db = None
+
 
 def init(server):
+    global server_in_db
+    try:
+        server_in_db = Server.objects.get(name=server.settings.server_name)
+    except Server.DoesNotExist:
+        server_in_db = Server(name=server.settings.server_name, running=True)
     # TODO load plugins from server
     # and then raise BreakPluginLoadingException
-    pass
+
+
+def load_settings():
+    return server_in_db.session_setups.all()
 
 
 def tick(server):
