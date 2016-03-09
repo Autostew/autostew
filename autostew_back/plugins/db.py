5917a01834731e84238e2880bd143e28c4fd5906
Joan Ardiaca <joan.ardiaca@misterspex.de>
2016-03-09 19:31:59 +0100

    Added a little type hinting

--- autostew_back/plugins/db.py
+++ autostew_back/plugins/db.py
@@ -1,7 +1,9 @@
+from autostew_back.gameserver.server import Server as DedicatedServer
+
 name = 'DB'
 
 
-def init(server):
+def init(server: DedicatedServer):
     import os
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "autostew.settings")
     from django.core.wsgi import get_wsgi_application
