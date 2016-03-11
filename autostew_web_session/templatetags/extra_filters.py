604a6f5b3498c95fa83ecd2f99ae2c584dccc5b7
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2016-03-12 00:32:36 +0100

    More work on session viewer

--- autostew_web_session/templatetags/extra_filters.py
+++ autostew_web_session/templatetags/extra_filters.py
@@ -13,3 +13,18 @@ def milli_to_nicetime(value):
     if value is None:
         return '---'
     return std_time_format(timedelta(milliseconds=value))
+
+
+@register.filter
+def temp(value):
+    return "{:.1f}".format(value/1000)
+
+
+@register.filter
+def pressure(value):
+    return "{:.0f}".format(value/100)
+
+
+@register.filter
+def percent(value):
+    return "{:.0f}".format(value/10)
