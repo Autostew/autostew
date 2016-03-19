8dab7e1f5b438634efaf8ae31a1473c3c06e51d6
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2016-03-19 04:41:35 +0100

    View sessions per user, initial server setup script

---
+++ make_me_a_server.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Makes a server (tested on ubuntu)
+
+# Set hostname
+sudo sed "1s/.*/127.0.0.1 localhost $1/" /etc/hosts
+sudo hostname $1
+echo $1 | sudo tee /etc/hostname
+
+# Install packages
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install python3-pip
+sudo pip install django=1.9
+sudo apt-get install lib32gcc1
+
+# Set up steam
+sudo useradd -m steam
+sudo -u steam mkdir /home/steam/steamcmd
+pushd /home/steam/steamcmd
+sudo -u steam wget https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
+sudo -u steam tar -xvzf steamcmd_linux.tar.gz
+sudo -u steam ./steamcmd.sh +login anonymous +force_install_dir ./pcars_ds +app_update 332670 validate +exit
+popd
+
+# Set up DS
+sudo -u steam ln -sfn /home/steam/steamcmd/pcars_ds/server.cfg server.cfg
+# TODO service unit
+
+# Set up apache TODO
+
+# Deploy Django TODO
+
+# Set up autostew TODO
+
+# Let's go! TODO
