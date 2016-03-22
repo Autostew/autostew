4e5fb9eaec9220892f79fe3c94ac6b81dea5658a
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2016-03-22 01:12:49 +0100

    Added recreate_db.sh to travis.yml

--- recreate_db.sh
+++ recreate_db.sh
@@ -2,5 +2,3 @@
 rm -vrf autostew_web_enums/migrations/* autostew_web_session/migrations/* autostew_web_users/migrations/* db.sqlite3
 ./manage.py makemigrations autostew_web_session autostew_web_enums autostew_web_users --settings=autostew.settings.dev
 ./manage.py migrate --settings=autostew.settings.dev
-echo "New admin password (username admin)"
-./manage.py createsuperuser --username=admin --email='none@localhost' --settings=autostew.settings.dev
