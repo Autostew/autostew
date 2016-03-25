7b02ff6de3db8fe4d8287f16893651a0ee4d5d5c
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2016-03-25 14:39:07 +0100

    Pimped MOTD plugin

--- autostew_back/plugins/motd.py
+++ autostew_back/plugins/motd.py
@@ -1,24 +1,40 @@
 """
-Show a message when a player logs in.
+Show a message when a player logs in (and other messages, too)
 """
-from autostew_back.gameserver.event import EventType
+from autostew_back.gameserver.event import EventType, BaseEvent
+from autostew_back.gameserver.server import Server
+from autostew_back.gameserver.session import SessionStage, SessionState
 
 name = 'motd'
 
-messages = [
+welcome_message = [
     "Welcome",
     "Current setup is {setup_name}",
 ]
+first_player_finished = [
+    "Congratulations to {winner_name} for winning this race!",
+    "See this race results and more about Autostew at http://autostew.selfhost.eu"
+]
+new_session_starts = [
+    "The next session is starting",
+    "Read more about Autostew at http://autostew.selfhost.eu"
+]
 
-def tick(server):
-    pass
 
+def event(server: Server, event:BaseEvent):
 
-def event(server, event):
     if event.type == EventType.authenticated:
-        for message in messages:
+        for message in welcome_message:
             event.member.send_chat(
                 message.format(
                     setup_name=server.get_current_setup_name()
                 )
             )
+
+    if event.type == EventType.results and event.race_position == 1 and server.session.session_stage == SessionStage.race1:
+        for message in first_player_finished:
+            server.api.send_chat(message.format(winner_name=event.participant.name.get()))
+
+    if event.type == EventType.state_changed and event.new_state == SessionState.lobby:
+        for message in new_session_starts:
+            server.api.send_chat(message)
