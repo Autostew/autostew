bd23aa19b63d4755cead0cbb34d8edb3c5421f7a
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2016-04-01 23:50:48 +0200

    Django admin models for users app

--- autostew_web_users/admin.py
+++ autostew_web_users/admin.py
@@ -2,5 +2,26 @@ from django.contrib import admin
 
 from autostew_web_users.models import SteamUser, SafetyClass
 
-admin.site.register(SteamUser)
-admin.site.register(SafetyClass)
+
+@admin.register(SteamUser)
+class SteamUserAdmin(admin.ModelAdmin):
+    list_filter = ['safety_class']
+    list_display = [
+        'display_name',
+        'steam_id',
+        'elo_rating',
+        'safety_rating',
+        'safety_class',
+    ]
+    search_fields = ['display_name', 'steam_id']
+
+
+@admin.register(SafetyClass)
+class SafetyClassAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'order',
+        'class_below',
+        'raise_to_this_class_threshold',
+        'drop_from_this_class_threshold'
+    ]
