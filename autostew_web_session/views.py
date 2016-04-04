7dc5eae1494af0d43a57a6ff3f1ca600780d0c2c
Michael Wittig <michael.wittig@fu-berlin.de>
2016-04-04 16:51:55 +0200

    Update views.py

--- autostew_web_session/views.py
+++ autostew_web_session/views.py
@@ -69,7 +69,7 @@ class SessionList(generic.ListView):
         context['sessions_in_progress'] = Session.objects.filter(running=True, finished=False)
         context['sessions_planned'] = Session.objects.filter(planned=True, running=False, finished=False)
         context['sessions_history'] = Session.objects.filter(finished=True).order_by('-id')[:10]
-        context['setups_in_rotation'] = SessionSetup.objects.filter(rotated_in_server=Server.objects.all())
+        context['setups_in_rotation'] = SessionSetup.objects.filter(rotated_in_server__in=Server.objects.all())
         return context
 
 
