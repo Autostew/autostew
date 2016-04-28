460cb146a651eb7c67d69ef58e07d7a203303c3e
Joan Ardiaca Jové <joan.ardiaca@gmail.com>
2016-04-28 23:49:16 +0200

    Dont allow setting server_controls_setup to false in SessionSetupForm

--- autostew_web_session/forms.py
+++ autostew_web_session/forms.py
@@ -10,11 +10,12 @@ class SessionSetupForm(ModelForm):
     def save(self, commit=True):
         session_setup = super(SessionSetupForm, self).save(commit=False)
         session_setup.is_template = True
+        session_setup.server_controls_setup = True
         if commit:
             session_setup.save()
 
         return session_setup
 
     class Meta:
-        exclude = ['is_template']
+        exclude = ['is_template', 'server_controls_setup']
         model = SessionSetup
