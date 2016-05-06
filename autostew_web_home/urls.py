dddd2e14cb32871db02be74b10de0d18442c1724
Michael Wittig <witmic1@gmail.com>
2016-05-06 10:21:06 +0200

    fixes url bug, where users could enter anything

--- autostew_web_home/urls.py
+++ autostew_web_home/urls.py
@@ -5,6 +5,6 @@ from autostew_web_home.views import HomeView
 
 urlpatterns = [
     url(r'^favicon\.ico$', RedirectView.as_view(url='/static/autostew_web_home/img/Logo_RGB.png', permanent=True)),
-    url(r'^faq', TemplateView.as_view(template_name='autostew_web_home/faq.html'), name='faq'),
-    url(r'^', HomeView.as_view(), name='home'),
+    url(r'^faq$', TemplateView.as_view(template_name='autostew_web_home/faq.html'), name='faq'),
+    url(r'^$', HomeView.as_view(), name='home'),
 ]
