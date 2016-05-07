ade9ef7d7e937648ed5ad2380ab34359003dd5a6
Michael Wittig <witmic1@gmail.com>
2016-05-07 09:55:20 +0200

    use default Modelserializer with all fields instead

--- autostew_web_api/serializers.py
+++ autostew_web_api/serializers.py
@@ -3,10 +3,8 @@ from rest_framework import serializers
 from autostew_web_session.models.session import Session
 
 
-class SessionSerializer(serializers.HyperlinkedModelSerializer):
-    parent = serializers.ReadOnlyField(source='session.parent')
+class SessionSerializer(serializers.ModelSerializer):
     children = serializers.PrimaryKeyRelatedField(many=True, queryset=Session.objects.all())
 
     class Meta:
         model = Session
-        fields = ('url', 'parent', 'children', 'finished', 'is_result', 'is_final_result', 'timestamp')
