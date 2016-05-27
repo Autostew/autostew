e7a6d066f4e6a91a412b6755a3bd5115d8f9c764
Michael Wittig <witmic1@gmail.com>
2016-05-27 09:41:06 +0200

    again debug but in problematic place

--- autostew_web_users/views.py
+++ autostew_web_users/views.py
@@ -1,3 +1,4 @@
+import sys
 from django.views import generic
 
 from autostew_web_users.models import SteamUser
@@ -7,6 +8,9 @@ class SteamUserListView(generic.ListView):
     model = SteamUser
 
     def get_queryset(self):
+
+        sys.stderr.write('\nin get_queryset' + '\n')
+
         name = self.request.GET.get("name")
         if name is not None and name != '':
             exact_search = self.model.objects.filter(display_name__exact=name)
@@ -31,6 +35,7 @@ class SteamUserListView(generic.ListView):
         return object_list
 
     def get_context_data(self, **kwargs):
+        sys.stderr.write('\n in get_context_data' + '\n')
         context = super(SteamUserListView, self).get_context_data(**kwargs)
         context['page'] = self.request.GET.get('page', '1')
         return context
